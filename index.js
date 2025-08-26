5e064195742698ea7c8a0cc4553edecfa47d5f9f
Revoj-a <revojjoever@gmail.com>
2025-08-26 17:53:46 +0800

    using aync and await for much cleaner code to fetch API

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-fetch("https://swapi.tech/api/people/1/")
-  .then((res) => {
-    console.log("RESOLVED", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("ERROR", err);
-  });
+// fetch("https://swapi.tech/api/people/1/")
+//   .then((res) => {
+//     console.log("RESOLVED", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     return fetch("https://swapi.tech/api/people/2/");
+//   })
+//   .then((res) => {
+//     console.log("SECOND REQUEST RESOLVED", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     return fetch("https://swapi.tech/api/people/3/");
+//   })
+//   .then((res) => {
+//     console.log("THIRD REQUEST RESOLVED", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR", err);
+//   });
+
+const loadStarWarsPeople = async () => {
+  const res = await fetch("https://swapi.tech/api/people/1/");
+  const data = await res.json();
+  console.log(data);
+};
+
+loadStarWarsPeople();
