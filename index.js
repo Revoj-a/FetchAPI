e820286824b0e2cc6bcf6894fefd61ca669b06eb
Revoj-a <revojjoever@gmail.com>
2025-08-26 17:57:36 +0800

    try and catch if something error occurs

--- index.js
+++ index.js
@@ -27,9 +27,17 @@
 //   });
 
 const loadStarWarsPeople = async () => {
-  const res = await fetch("https://swapi.tech/api/people/1/");
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch("https://swapi.tech/api/people/1/");
+    const data = await res.json();
+    console.log(data);
+
+    const res2 = await fetch("https://swapi.tech/api/people/2/");
+    const data2 = await res2.json();
+    console.log(data2);
+  } catch (e) {
+    console.log("ERROR", e);
+  }
 };
 
 loadStarWarsPeople();
