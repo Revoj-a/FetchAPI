d69933c10336db6cedde0cc5b9f3c6e80b3e2770
Revoj-a <revojjoever@gmail.com>
2025-08-26 17:40:56 +0800

    using .json to retrieve data

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 fetch("https://swapi.tech/api/people/1/")
   .then((res) => {
     console.log("RESOLVED", res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
   })
   .catch((err) => {
     console.log("ERROR", err);
